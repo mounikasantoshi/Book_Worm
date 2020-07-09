6645916333e1c9ab37fc020c918bb4ebd6e5ffaf
mounika <mounikasantoshi99@gail.com>
2020-07-10 01:23:48 +0530

    next commit

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../../actions/auth';
 
-const HomePage = () => (
+const HomePage = ({ isAuthentiscated, logout }) => (
   <div>
     <h1>Home Page</h1>
-    <Link to="/login">Login</Link>
+    {isAuthentiscated ? (
+      <button onClick={() => logout()}>Logout</button>
+    ) : (
+      <Link to="/login">Login</Link>
+    )}
   </div>
 );
 
-export default HomePage;
+HomePage.propTypes = {
+  isAuthentiscated: propTypes.bool.isRequired,
+  logout: propTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    isAuthentiscated: !!state.user.token,
+  };
+}
+
+export default connect(mapStateToProps, { logout: actions.logout })(HomePage);
